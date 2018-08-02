7ab523e54e1d6b8779ea973a31fee915a561ac25
Unknown <asf@click-solutions.de>
2018-08-02 19:29:25 +0200

    feat: learning reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MenuBarComponent } from './components/menubar/menubar.component';
@@ -30,6 +30,7 @@ export function getToken() {
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     JwtModule.forRoot({
       config: {
--- src/app/components/protected/protected.component.ts
+++ src/app/components/protected/protected.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../hero';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-protected',
   templateUrl: './protected.component.html'
 })
 export class ProtectedComponent implements OnInit {
+
+  name: FormControl = new FormControl('');
 
   constructor() { }
 
